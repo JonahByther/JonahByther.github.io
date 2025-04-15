358123d1e27e71bb970036d64d9a3c38c74f6c81
Jonah Byther <86095166+JonahByther@users.noreply.github.com>
2025-04-14 19:31:36 -0700

    Update GetExperience.js

--- Scripts/GetExperience.js
+++ Scripts/GetExperience.js
@@ -32,6 +32,9 @@ function updateHTML(configData) {
   document.querySelector("#pfSkills").innerHTML = configData.Experience.Skills.toString();
   document.querySelector("#pfSkillsText").innerHTML = configData.Experience.SkillsText;
 
+  document.querySelector("#expCertification>h2").innerHTML = "Certifications";
+  document.querySelector("#pfCertifications").innerHTML = configData.Experience.Skills.toString();
+
   // document.querySelector("#pfResume").href = "./Content/".concat(configData.Experience.Resume);
   // document.querySelector("#pfResume").href = "./Content/resume.pdf";
   document.querySelector("#pfResume").onclick = () => {
